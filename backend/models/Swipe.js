f06476b781937d1462a66357d1bb0f3146ad82e1
Bastiesp <bastian.ef89@gmail.com>
2026-04-28 20:07:25 -0400

    Add files via upload

--- backend/models/Swipe.js
+++ backend/models/Swipe.js
@@ -4,21 +4,26 @@ const swipeSchema = new mongoose.Schema({
   swiper: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    required: true,
+    index: true
   },
   book: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Book',
-    required: true
+    required: true,
+    index: true
   },
   direction: {
     type: String,
     enum: ['right', 'left'],
-    required: true
+    required: true,
+    index: true
   }
 }, { timestamps: true });
 
-// Un usuario solo puede deslizar cada libro una vez
 swipeSchema.index({ swiper: 1, book: 1 }, { unique: true });
+swipeSchema.index({ swiper: 1, direction: 1 });
+swipeSchema.index({ book: 1, direction: 1 });
+swipeSchema.index({ swiper: 1, book: 1, direction: 1 });
 
 module.exports = mongoose.model('Swipe', swipeSchema);
